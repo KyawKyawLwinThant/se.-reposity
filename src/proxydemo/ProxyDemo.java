dfc434c6c68ee7ee1963461425061268f29b8537
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2017-12-22 09:51:04 +0630

    comment 2

--- src/proxydemo/ProxyDemo.java
+++ src/proxydemo/ProxyDemo.java
@@ -13,7 +13,7 @@ public class ProxyDemo {
 
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World" + "This is amazing...");
     }
     
 }
